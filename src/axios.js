eb6b5d38ac5714f250394bedfa7009a5fec1b40d
aaog2 <aungaungoogame2@gmail.com>
2024-07-31 22:44:01 +0630

    feat: add rest password

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "/api";
+const BASE_URL = import.meta.env.VITE_API_URL;
 const token = localStorage.getItem("authToken");
 // let usertoken = localStorage.getItem("token");
 axios.defaults.baseURL = BASE_URL;
